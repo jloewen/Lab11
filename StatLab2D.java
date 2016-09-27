3ccdc4b897081fa142a68b818dd045d0454b53b5
jloewen <jloewen445@gmail.com>
2016-09-27 08:45:08 -0700

    Add files via upload

---
+++ StatLab2D.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class StatLab2d here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StatLab2d
+{
+   public static void main (String args[]){
+     int rows = 7;
+     int cols = 9;
+     int[][] nums = new int[rows][cols];
+     StatLab2d pass2D = new StatLab2d(inums);
+    }
+   public int intloadArray2D(){
+       
+    }
+   public int printArray2d(){
+       
+    }
+   public int sortArray(){
+       
+    }
+   public int getMean2D(){
+       
+    }
+}
